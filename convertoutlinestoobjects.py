06542bf7fcefd96e68e07e1f28eda7ea6e97f06a
mcquin <mcquin@broadinstitute.org>
2018-01-25 14:10:41 -0500

    Add option to filter objects by size

--- convertoutlinestoobjects.py
+++ convertoutlinestoobjects.py
@@ -4,11 +4,8 @@
 ConvertOutlinesToObjects
 =====================
 
-**ConvertOutlinesToObjects** converts a binary image of outlines to objects. Contiguous regions are converted to
-unique objects. Note that the background of the image will be detected as an object. Use **FilterObjects** to remove the
-background region. Typically, the background object is much larger than the actual objects. Use a size threshold to
-remove any large contiguous regions representing background. Occasionally, small (< 5) pixel background regions are
-identified as objects. Use a size threshold to exclude any tiny contiguous regions representing background.
+**ConvertOutlinesToObjects** converts a binary image of outlines to objects. Contiguous outlined regions are converted
+to unique objects.
 
 |
 
@@ -17,13 +14,15 @@ Supports 2D? Supports 3D? Respects masks?
 ============ ============ ===============
 YES          YES          NO
 ============ ============ ===============
-
 """
 
+import numpy
+import scipy.ndimage
 import skimage
 import skimage.measure
 
 import cellprofiler.module
+import cellprofiler.setting
 
 
 class ConvertOutlinesToObjects(cellprofiler.module.ImageSegmentation):
@@ -33,11 +32,70 @@ class ConvertOutlinesToObjects(cellprofiler.module.ImageSegmentation):
 
     variable_revision_number = 1
 
-    def run(self, workspace):
-        self.function = lambda x_data: skimage.measure.label(
-            skimage.img_as_bool(x_data),
-            background=True,
-            connectivity=1
+    def create_settings(self):
+        super(ConvertOutlinesToObjects, self).create_settings()
+
+        self.diameter = cellprofiler.setting.FloatRange(
+            text="Typical diameter of objects",
+            value=(0.0, numpy.inf),
+            doc="Typical diameter of objects, in pixels (min, max). Objects outside this range will be discarded."
         )
 
+    def settings(self):
+        settings = super(ConvertOutlinesToObjects, self).settings()
+
+        settings += [
+            self.diameter
+        ]
+
+        return settings
+
+    def visible_settings(self):
+        visible_settings = super(ConvertOutlinesToObjects, self).visible_settings()
+
+        visible_settings += [
+            self.diameter
+        ]
+
+        return visible_settings
+
+    def run(self, workspace):
+        self.function = convert_outlines_to_objects
+
         super(ConvertOutlinesToObjects, self).run(workspace)
+
+
+def convert_outlines_to_objects(outlines, diameter):
+    labels = skimage.measure.label(
+        outlines > 0,
+        background=True,
+        connectivity=1
+    )
+
+    indexes = numpy.unique(labels)
+
+    radius = numpy.divide(diameter, 2.0)
+
+    if labels.ndim == 2:
+        factor = radius ** 2
+    else:
+        factor = (4.0 / 3.0) * (radius ** 3)
+
+    min_area, max_area = numpy.pi * factor
+
+    areas = scipy.ndimage.sum(
+        numpy.ones_like(labels),
+        labels,
+        index=indexes
+    )
+
+    is_background = numpy.logical_or(
+        areas < min_area,
+        areas > max_area
+    )
+
+    background_indexes = numpy.unique(labels)[is_background]
+
+    labels[numpy.isin(labels, background_indexes)] = 0
+
+    return labels
