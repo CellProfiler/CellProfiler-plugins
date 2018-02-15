ad04708dffae0d0cc943e39385ba60392de583da
mcquin <mcquincl@gmail.com>
2018-02-15 12:21:19 -0600

    get the settings

--- runimagej.py
+++ runimagej.py
@@ -1,4 +1,6 @@
 import cellprofiler.module
+import cellprofiler.setting
+import imagej
 
 
 # To start CellProfiler with the plugins directory:
@@ -6,6 +8,49 @@ import cellprofiler.module
 class RunImageJ(cellprofiler.module.ImageProcessing):
     module_name = "RunImageJ"
     variable_revision_number = 1
+
+    @staticmethod
+    def get_ij_modules():
+        # TODO: configure the ImageJ server host in CellProfiler's preferences.
+        # Not sure we can do this via CellProfiler-plugins but maaaaaybeeee?
+        # Otherwise, we'll need to expose this via a setting or an env variable.
+        #
+        # I tried exposing this through a setting and calling on_setting_changed
+        # when the host was updated; on_setting_change would repopulate the
+        # module choices using this method. However, there isn't a choices setter
+        # on the Choice setting. :(
+        ij = imagej.IJ()
+        modules = ij.modules()
+        modules = [module.split(".")[-1] for module in modules]
+        return modules
+
+    def on_setting_changed(self, setting, pipeline):
+        if not setting == self.ijmodule:
+            return
+
+        self.create_ijsettings(setting.value)
+
+    def create_ijsettings(self, module_name):
+        self.ijsettings = cellprofiler.setting.SettingsGroup()
+
+        # Get the module and the module details
+        ij = imagej.IJ()
+        module = ij.find(module_name)[0]
+        details = ij.detail(module)
+        inputs = details["inputs"]
+
+        # # FOR DEBUGGING
+        # import json
+        # print(json.dumps(details, indent=4))
+
+        for input_ in inputs:
+            name = input_["name"]
+            default_value = input_["defaultValue"]
+
+            self.ijsettings.append(
+                name,
+                cellprofiler.setting.Text(name, str(default_value))
+            )
 
     # Define settings as instance variables
     # Available settings are in in cellprofiler.settings
@@ -16,6 +61,13 @@ class RunImageJ(cellprofiler.module.ImageProcessing):
     def create_settings(self):
         super(RunImageJ, self).create_settings()
 
+        self.ijmodule = cellprofiler.setting.Choice(
+            "ImageJ module",
+            choices=self.get_ij_modules()
+        )
+
+        self.create_ijsettings(self.ijmodule.value)
+
     # Returns the list of available settings
     # This is primarily used to load/save the .cppipe/.cpproj files
     #
@@ -24,6 +76,9 @@ class RunImageJ(cellprofiler.module.ImageProcessing):
     def settings(self):
         settings = super(RunImageJ, self).settings()
 
+        settings.append(self.ijmodule)
+        settings += self.ijsettings.settings
+
         return settings
 
     # Returns a list of settings which are available to the GUI.
@@ -31,6 +86,9 @@ class RunImageJ(cellprofiler.module.ImageProcessing):
     # to activate and deactivate GUI options.
     def visible_settings(self):
         visible_settings = super(RunImageJ, self).visible_settings()
+
+        visible_settings.append(self.ijmodule)
+        visible_settings += self.ijsettings.settings
 
         return visible_settings
 
