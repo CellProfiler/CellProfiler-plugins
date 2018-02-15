733658a47b67a97fc2777450100658192ffb7c69
Curtis Rueden <ctrueden@wisc.edu>
2018-02-15 14:29:16 -0600

    Map ImageJ input types to CellProfiler settings
    Woo, case logic!

--- runimagej.py
+++ runimagej.py
@@ -2,6 +2,55 @@ import cellprofiler.module
 import cellprofiler.setting
 import imagej
 
+BOOL_TYPES = [
+    "boolean",
+    "java.lang.Boolean"
+]
+
+COLOR_TYPES = ["org.scijava.util.ColorRGB"]
+
+FILE_TYPES = ["java.io.File"]
+
+FLOAT_TYPES = [
+    "double",
+    "float",
+    "java.lang.Double",
+    "java.lang.Float",
+    "java.math.BigDecimal"
+]
+
+IGNORE_TYPES = [
+    "org.scijava.widget.Button"  # For now!
+]
+
+IMAGE_TYPES = [
+    "net.imagej.Dataset",
+    "net.imagej.display.DataView",
+    "net.imagej.display.DatasetView",
+    "net.imagej.display.ImageDisplay",
+    "net.imglib2.IterableInterval",
+    "net.imglib2.RandomAccessibleInterval",
+    "net.imglib2.img.*Img"
+]
+
+INTEGER_TYPES = [
+    "byte",
+    "int",
+    "long",
+    "short",
+    "java.lang.Byte",
+    "java.lang.Integer",
+    "java.lang.Long",
+    "java.lang.Short",
+    "java.math.BigInteger"
+]
+
+TEXT_TYPES = [
+    "char",
+    "java.lang.Character",
+    "java.lang.String",
+    "java.util.Date"
+]
 
 # To start CellProfiler with the plugins directory:
 #   `pythonw -m cellprofiler --plugins-directory .`
@@ -53,7 +102,6 @@ class RunImageJ(cellprofiler.module.ImageProcessing):
         for input_ in inputs:
             name = input_["name"]
             default_value = input_["defaultValue"]
-
             input_["rawType"] = raw_type = input_["genericType"].split("<")[0].split(" ")[-1]
 
             # HACK: For now, we skip service and context parameters.
@@ -66,10 +114,65 @@ class RunImageJ(cellprofiler.module.ImageProcessing):
             #   But ImageJ server does not tell us right now
             # - Add outputs
 
-            self.ijsettings.append(
-                name,
-                cellprofiler.setting.Text(name, str(default_value))
-            )
+            setting = self.make_setting(input_)
+            if setting is not None:
+                self.ijsettings.append(name, setting)
+
+    def make_setting(self, input_):
+        raw_type = input_["rawType"]
+        if raw_type in IGNORE_TYPES:
+            # print("**** Ignoring input: '" + input_["name"] + "' of type '" + raw_type + "' ****")
+            return None
+
+        label = input_["label"]
+        text = label if not (label is None or label == "") else input_["name"]
+        value = input_["defaultValue"]
+        minval = input_["minimumValue"]
+        maxval = input_["maximumValue"]
+        style = input_["widgetStyle"].lower()
+
+        if raw_type in BOOL_TYPES:
+            return cellprofiler.setting.Binary(text, value)
+
+        if raw_type in COLOR_TYPES:
+            return cellprofiler.setting.Color(text, value)
+
+        if raw_type in FILE_TYPES:
+            if style.startswith("directory"):
+                # TODO: Massage non-None value to CellProfiler-friendly string.
+                return cellprofiler.setting.DirectoryPath(text)
+
+            # "open" or "save" or unspecified
+            # TODO: Use a fancier combination of widgets.
+            return cellprofiler.setting.Pathname(text, value if value else '')
+
+        if raw_type in FLOAT_TYPES:
+            return cellprofiler.setting.Float(text, self.clamp(value, minval), minval, maxval)
+
+        if raw_type in IMAGE_TYPES:
+            return cellprofiler.setting.ImageNameSubscriber(text)
+
+        if raw_type in INTEGER_TYPES:
+            return cellprofiler.setting.Integer(text, self.clamp(value, minval), minval, maxval)
+
+        if raw_type in TEXT_TYPES:
+            choices = input_["choices"]
+            if choices is None:
+                if style.startswith("text area"):
+                    return cellprofiler.setting.Text(text, value, multiline=True)
+                return cellprofiler.setting.Text(text, value)
+
+            return cellprofiler.setting.Choice(text, choices)
+
+        # TODO: handle error somehow -- maybe put a label saying "unsupported input: blah"
+        # print("**** Unsupported input: '" + input_["name"] + "' of type '" + raw_type + "' ****")
+        return None
+
+    def clamp(self, value, minval):
+        if value:
+            return value
+
+        return minval if minval else 0
 
     # Define settings as instance variables
     # Available settings are in in cellprofiler.settings
