bf0f6846ec0bd8e8fa6d1b7ffbfaa4acdcb7d76c
Curtis Rueden <ctrueden@wisc.edu>
2018-02-15 15:00:19 -0600

    Make RunImageJ module a vanilla module

--- runimagej.py
+++ runimagej.py
@@ -57,7 +57,8 @@ TEXT_TYPES = [
 
 # To start CellProfiler with the plugins directory:
 #   `pythonw -m cellprofiler --plugins-directory .`
-class RunImageJ(cellprofiler.module.ImageProcessing):
+class RunImageJ(cellprofiler.module.Module):
+    category = "Advanced"
     module_name = "RunImageJ"
     variable_revision_number = 1
 
@@ -179,13 +180,7 @@ class RunImageJ(cellprofiler.module.ImageProcessing):
 
     # Define settings as instance variables
     # Available settings are in in cellprofiler.settings
-    #
-    # The superclass creates the following settings:
-    #   - self.x_name: cellprofiler.setting.ImageNameSubscriber, the input image
-    #   - self.y_name: cellprofiler.setting.ImageNameProvider, the output image
     def create_settings(self):
-        super(RunImageJ, self).create_settings()
-
         self.ijmodule = cellprofiler.setting.Choice(
             "ImageJ module",
             choices=self.get_ij_modules()
@@ -195,29 +190,11 @@ class RunImageJ(cellprofiler.module.ImageProcessing):
 
     # Returns the list of available settings
     # This is primarily used to load/save the .cppipe/.cpproj files
-    #
-    # The superclass returns:
-    #   - [self.x_name, self.y_name]
     def settings(self):
-        settings = super(RunImageJ, self).settings()
-
-        settings.append(self.ijmodule)
+        settings = [self.ijmodule]
         settings += self.ijsettings.settings
 
         return settings
 
-    # Returns a list of settings which are available to the GUI.
-    # By default, this is `settings`. Conditional logic can be used
-    # to activate and deactivate GUI options.
-    def visible_settings(self):
-        visible_settings = super(RunImageJ, self).visible_settings()
-
-        visible_settings.append(self.ijmodule)
-        visible_settings += self.ijsettings.settings
-
-        return visible_settings
-
     def run(self, workspace):
-        self.function = lambda x: x
-
-        super(RunImageJ, self).run(workspace)
+        pass
