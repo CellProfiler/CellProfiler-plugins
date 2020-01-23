c0028d62188f92268b2d7af39c5aa5f7eab5683d
Beth Cimini <beth.cimini@gmail.com>
2020-01-23 09:35:13 -0500

    add ability to add an empty vector code to the pipeline

--- callbarcodes.py
+++ callbarcodes.py
@@ -215,6 +215,18 @@ This measurement should be """)
             doc="""\
 """)
 
+        self.has_empty_vector_barcode = cellprofiler.setting.Binary(
+            "Do you have an empty vector barcode you would like to add to the barcode list?", False, doc="""\
+Select "*{YES}*" to manually enter a sequence that should be added to the uploaded barcode 
+list with the gene name of "EmptyVector". This can be helpful when there is a consistent
+backbone sequence to look out for in every barcoding set).""" .format(**{"YES": cellprofiler.setting.YES
+                       }))
+
+        self.empty_vector_barcode_sequence = cellprofiler.setting.Text(
+            "What is the empty vector sequence?", "AAAAAAAAAAAAAAA", doc="""\
+Enter the sequence that represents barcoding reads of an empty vector"""
+        )
+
         self.wants_call_image = cellprofiler.setting.Binary(
             "Retain an image of the barcodes color coded by call?", False, doc="""\
 Select "*{YES}*" to retain the image of the objects color-coded
@@ -263,6 +275,8 @@ Enter the name to be given to the barcode score image.""")
             self.csv_file_name,
             self.metadata_field_barcode,
             self.metadata_field_tag,
+            self.has_empty_vector_barcode,
+            self.empty_vector_barcode_sequence,
             self.wants_call_image,
             self.outimage_calls_name,
             self.wants_score_image,
@@ -278,12 +292,17 @@ Enter the name to be given to the barcode score image.""")
             self.csv_file_name,
             self.metadata_field_barcode,
             self.metadata_field_tag,
-            self.wants_call_image,
-            self.wants_score_image
-        ]
+            self.has_empty_vector_barcode]
+
+        if self.has_empty_vector_barcode:
+            result += [self.empty_vector_barcode_sequence]
+
+        result += [self.wants_call_image]
 
         if self.wants_call_image:
             result += [self.outimage_calls_name]
+
+        result += [self.wants_score_image]
 
         if self.wants_score_image:
             result += [self.outimage_score_name]
@@ -521,6 +540,9 @@ Enter the name to be given to the barcode score image.""")
             barcodeset[row[barcodecol]]=(count,row[genecol])
             count+=1
         fd.close()
+        if self.has_empty_vector_barcode:
+            barcodeset[self.empty_vector_barcode_sequence.value]=(count,"EmptyVector")
+
         return barcodeset
 
     def likelihood(self,barcode,query):
