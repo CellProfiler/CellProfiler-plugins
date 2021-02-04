151bf553a64da106f00b9a3f6a73ff81518095b1
Mark Hiner <hinerm@gmail.com>
2021-02-04 11:16:18 -0600

    Enable script execution

--- runimagejscript.py
+++ runimagejscript.py
@@ -28,6 +28,7 @@ import skimage.io
 Constants for communicating with pyimagej
 """
 pyimagej_cmd_script_params = "COMMAND_SCRIPT_PARAMS"  # Parse the following script file's parameters
+pyimagej_cmd_script_run = "COMMAND_SCRIPT_RUN"  # Run an indicated script
 pyimagej_cmd_exit = "COMMAND_EXIT"  # Shut down the pyimagej daemon
 pyimagej_status_running = "STATUS_RUNNING"  # Returned when a command starts running
 pyimagej_status_cmd_unknown = "STATUS_COMMAND_UNKNOWN"  # Returned when an unknown command is passed to pyimagej
@@ -42,7 +43,6 @@ class ScriptFilename(Filename):
     optional arguments -
        value_change_fn is a function that gets called when the file value changes
     """
-
     def __init__(self, text, value, *args, **kwargs):
         kwargs = kwargs.copy()
         self.value_change_fn = kwargs.pop("value_change_fn", None)
@@ -53,7 +53,42 @@ class ScriptFilename(Filename):
         self.value_change_fn()
 
 
-def make_equivalent_setting(param_name, param_type):
+def convert_java_to_python_type(return_value):
+    """
+    Helper method to convert ImageJ/Java values to python values that can be passed between via queues (pickled)
+
+    Parameters
+    ----------
+    return_value : supported Java type, required
+        A value to convert from Java to python
+
+    Returns
+    ---------
+    An instance of a python type that can safely cross queues with the given value, or None if no valid type exists.
+    """
+    type_string = str(return_value.getClass().toString()).split()[1]
+    if type_string == "java.lang.String" or type_string == "java.lang.Character":
+        return str(return_value)
+    elif type_string == "java.lang.Integer" or type_string == "java.lang.Long" or type_string == "java.lang.Short":
+        return int(return_value)
+    elif type_string == "java.lang.Float" or type_string == "java.lang.Double":
+        return float(return_value)
+    elif type_string == "java.lang.Boolean":
+        if return_value:
+            return True
+        else:
+            return False
+    elif type_string == "java.lang.Byte":
+        return bytes(return_value)
+    elif type_string == "net.imagej.Dataset" or type_string == "net.imagej.ImgPlus":
+        # FIXME use pyimagej convert
+        return ImageSubscriber(return_value)
+
+    # Not a supported type
+    return None
+
+
+def convert_java_type_to_setting(param_name, param_type):
     """
     Helper method to convert ImageJ/Java class parameter types to CellProfiler settings
 
@@ -69,13 +104,12 @@ def make_equivalent_setting(param_name, param_type):
     A new Setting of a type appropriate for param_type, named with param_name. Or None if no valid conversion exists.
     """
     type_string = param_type.split()[1]
-    if type_string == "java.lang.String":
+    if type_string == "java.lang.String" or type_string == "java.lang.Character":
         return Alphanumeric(param_name)
-    elif type_string == "java.lang.Integer" or type_string == "java.lang.Long" or type_string == "java.lang.Short" or \
-            type_string == "int" or type_string == "long" or type_string == "short":
+    elif type_string == "java.lang.Integer" or type_string == "java.lang.Long" or type_string == "java.lang.Short" \
+            or type_string == "java.lang.Boolean" or type_string == "java.lang.Byte":
         return Integer(param_name)
-    elif type_string == "java.lang.Float" or type_string == "java.lang.Double" or type_string == "float" or \
-            type_string == "double":
+    elif type_string == "java.lang.Float" or type_string == "java.lang.Double":
         return Float(param_name)
     elif type_string == "java.io.File":
         return Filename(param_name)
@@ -96,8 +130,11 @@ def start_imagej_process(input_queue, output_queue):
     Supported commands
     ----------
     {pyimagej_cmd_script_params} : parse the parameters from an imagej script. 
-        inputs: script_filename
+        inputs: script filename
         outputs: parameter name/value pairs
+    {pyimagej_cmd_script_run} : takes a set of named inputs from CellProfiler and runs the given imagej script
+        inputs: script filename, input parameter name/value dictionary
+        outputs: output field name/value dictionary
     {pyimagej_cmd_exit} : shut down the pyimagej daemon
         inputs: none
         outputs:none
@@ -138,6 +175,21 @@ def start_imagej_process(input_queue, output_queue):
                 output_queue.put(str(script_out.getName()))
                 output_queue.put(str(script_out.getType().toString()))
             output_queue.put(pyimagej_status_cmd_done)
+        elif cmd == pyimagej_cmd_script_run:
+            output_queue.put(pyimagej_status_running)
+            script_path = input_queue.get()
+            script_file = File(script_path)
+            input_map = input_queue.get()
+            # FIXME probably need to convert input python types
+            script_out_map = script_service.run(script_file, True, input_map).get().getOutputs()
+            output_dict = {}
+            for entry in script_out_map.entrySet():
+                key = str(entry.getKey())
+                value = convert_java_to_python_type(entry.getValue())
+                if value is not None:
+                    output_dict[key] = value
+
+            output_queue.put(output_dict)
         elif cmd == pyimagej_cmd_exit:
             break
         else:
@@ -159,12 +211,12 @@ class RunImageJScript(Module):
 
     def __init__(self):
         super().__init__()
-        self.imagej_process = None
-        self.to_imagej = None
-        self.from_imagej = None
-        self.parsed_params = False
-        self.script_inputs = {}
-        self.script_outputs = {}
+        self.imagej_process = None  # A subprocess running pyimagej
+        self.to_imagej = None  # Queue to pass data to pyimagej
+        self.from_imagej = None  # queue to receive data from pyimagej
+        self.parsed_params = False  # Used for validation
+        self.script_input_settings = {}  # Map of input parameter names to CellProfiler settings objects
+        self.script_output_settings = {}  # Map of output parameter names to CellProfiler settings objects
 
     def create_settings(self):
         self.script_directory = Directory(
@@ -247,8 +299,8 @@ Note: this must be done each time you change the script, before running the Cell
         Remove any existing settings added by scripts
         """
         self.script_parameter_list.clear()
-        self.script_inputs = {}
-        self.script_outputs = {}
+        self.script_input_settings = {}
+        self.script_output_settings = {}
         self.parsed_params = False
         pass
 
@@ -274,7 +326,7 @@ Note: this must be done each time you change the script, before running the Cell
 
         # Process pyimagej's output
         if ij_return != pyimagej_status_cmd_unknown:
-            param_dict = self.script_inputs
+            param_dict = self.script_input_settings
             added_divider = False
             while True:
                 group = SettingsGroup()
@@ -286,15 +338,13 @@ Note: this must be done each time you change the script, before running the Cell
 
                 # Check if input parsing is complete
                 if param_name == pyimagej_script_outputs:
-                    param_dict = self.script_outputs
-                    group.append("value", Divider(line=True))
-                    self.script_parameter_list.append(group)
+                    param_dict = self.script_output_settings
                     added_divider = False
                     continue
 
-                next_setting = make_equivalent_setting(param_name, self.from_imagej.get())
-                param_dict[param_name] = next_setting
+                next_setting = convert_java_type_to_setting(param_name, self.from_imagej.get())
                 if next_setting is not None:
+                    param_dict[param_name] = next_setting
                     group.append("value", next_setting)
                     if not added_divider:
                         self.add_divider()
@@ -311,6 +361,24 @@ Note: this must be done each time you change the script, before running the Cell
         pass
 
     def run(self, workspace):
+        script_filepath = path.join(self.script_directory.get_absolute_path(), self.script_file.value)
+
+        # convert the CP settings to script parameters for pyimagej
+        script_inputs = {}
+        for name in self.script_input_settings:
+            script_inputs[name] = self.script_input_settings[name].get_value()
+
+        # Start the script
+        self.to_imagej.put(pyimagej_cmd_script_run)
+        self.to_imagej.put(script_filepath)
+        self.to_imagej.put(script_inputs)
+
+        # Retrieve script output
+        ij_return = self.from_imagej.get()
+        if ij_return != pyimagej_status_cmd_unknown:
+            print("output received")
+            # TODO update output settings
+
         pass
 
     def display(self, workspace, figure):
