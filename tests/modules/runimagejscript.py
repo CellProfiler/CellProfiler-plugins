f74fbd923dc695378be5bb88c38e7738d1eee435
alucas <alicelucas93@gmail.com>
2021-03-31 11:09:55 -0400

    Empty tests

---
+++ tests/modules/runimagejscript.py
@@ -0,0 +1,40 @@
+import numpy
+
+import cellprofiler_core.image
+import cellprofiler_core.measurement
+from cellprofiler_core.constants.measurement import GROUP_INDEX, GROUP_NUMBER, COLTYPE_INTEGER
+
+
+import cellprofiler.modules.crop
+import cellprofiler_core.object
+import cellprofiler_core.pipeline
+import cellprofiler_core.workspace
+
+INPUT_IMAGE = "input_image"
+CROP_IMAGE = "crop_image"
+CROP_OBJECTS = "crop_objects"
+CROPPING = "cropping"
+OUTPUT_IMAGE = "output_image"
+
+
+def make_workspace():
+    """Return a workspace with the given images installed and the runimagejscript module"""
+    pass
+
+def test_parse_parameters():
+    pass
+
+def test_start_image_j():
+    pass
+
+def test_invalid_script():
+    pass
+
+def test_do_nothing_to_image():
+    pass
+
+def test_do_something_to_image():
+    pass
+
+def test_multiple_output_images():
+    pass
