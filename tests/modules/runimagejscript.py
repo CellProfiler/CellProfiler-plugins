9accce672ecfbdfef181ed0b718cc87b3c31c687
alucas <alicelucas93@gmail.com>
2021-03-31 11:23:28 -0400

    test imagej process

--- tests/modules/runimagejscript.py
+++ tests/modules/runimagejscript.py
@@ -10,6 +10,8 @@ import cellprofiler_core.object
 import cellprofiler_core.pipeline
 import cellprofiler_core.workspace
 
+import cellprofiler.modules.runimagejscript
+
 INPUT_IMAGE = "input_image"
 CROP_IMAGE = "crop_image"
 CROP_OBJECTS = "crop_objects"
@@ -18,13 +20,40 @@ OUTPUT_IMAGE = "output_image"
 
 
 def make_workspace():
-    """Return a workspace with the given images installed and the runimagejscript module"""
+    """Return a workspace with the given image and the runimagejscript module"""
+    image_set_list = cellprofiler_core.image.ImageSetList()
+    image_set = image_set_list.get_image_set(0)
+
+    module = cellprofiler.modules.runimagejscript.RunImageJScript()
+    module.set_module_num(1)
+
+    #FIXME: here set the module attributes: script directory and file
+
+    object_set = cellprofiler_core.object.ObjectSet()
+
+    pipeline = cellprofiler_core.pipeline.Pipeline()
+
+    def callback(caller, event):
+        assert not isinstance(event, cellprofiler_core.pipeline.event.RunException)
+
+    pipeline.add_listener(callback)
+    pipeline.add_module(module)
+
+    m = cellprofiler_core.measurement.Measurements()
+
+    workspace = cellprofiler_core.workspace.Workspace(
+        pipeline, module, image_set, object_set, m, image_set_list
+    )
+
+    return workspace, module
+
+def test_start_image_j():
+    workspace, module = make_workspace()
+    module.init_pyimagej()
+    module.close_pyimagej()
     pass
 
 def test_parse_parameters():
-    pass
-
-def test_start_image_j():
     pass
 
 def test_invalid_script():
