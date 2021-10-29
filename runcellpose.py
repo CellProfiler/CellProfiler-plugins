306143d6deeda1436a5709577c648e188fee5e38
Rebecca Senft <senft@g.harvard.edu>
2021-10-29 14:17:33 -0400

    Try limiting memory to GPUs

--- runcellpose.py
+++ runcellpose.py
@@ -237,8 +237,17 @@ Cell probability threshold (all pixels with probability above threshold kept for
 
 
     def run(self, workspace):
-        lock=threading.Lock()
-        lock.acquire()
+        import tensorflow as tf
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+            try:
+                tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=6000)])
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            except RuntimeError as e:
+                # Virtual devices must be set before GPUs have been initialized
+                print(e)
         if self.mode.value != MODE_CUSTOM:
             self.model = models.Cellpose(model_type='cyto' if self.mode.value == MODE_CELLS else 'nuclei',
                                     gpu=self.use_gpu.value)
