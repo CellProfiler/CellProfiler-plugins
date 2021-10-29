3c0834a83211640b6c171331ab063f6da01044d2
Rebecca Senft <senft@g.harvard.edu>
2021-10-29 13:36:42 -0400

    Trying out thread locking to prevent memory issue

--- runcellpose.py
+++ runcellpose.py
@@ -12,7 +12,7 @@ from cellprofiler_core.setting.do_something import DoSomething
 from cellprofiler_core.setting.subscriber import ImageSubscriber
 from cellprofiler_core.setting.text import Integer, ImageName, Directory, Filename, Float
 
-
+import threading
 
 CUDA_LINK = "https://pytorch.org/get-started/locally/"
 
@@ -49,6 +49,7 @@ MODE_CUSTOM = "Custom"
 
 
 class RunCellpose(ImageSegmentation):
+    lock=threading.Lock()
     category = "Object Processing"
 
     module_name = "RunCellpose"
@@ -235,7 +236,8 @@ Cell probability threshold (all pixels with probability above threshold kept for
         return vis_settings
 
 
-    def run(self, workspace):
+    def run(self, workspace, lock):
+        lock.acquire()
         if self.mode.value != MODE_CUSTOM:
             self.model = models.Cellpose(model_type='cyto' if self.mode.value == MODE_CELLS else 'nuclei',
                                     gpu=self.use_gpu.value)
@@ -268,67 +270,65 @@ Cell probability threshold (all pixels with probability above threshold kept for
 
         diam = self.expected_diameter.value if self.expected_diameter.value > 0 else None
         from torch import cuda
-        from time import sleep
         print(cuda.memory_reserved())
         print(cuda.get_device_properties(0).total_memory/2)
-        while cuda.memory_reserved()>=30000000:
-            print("worker waiting for GPU memory...")
-            sleep(5)
-        else:
-            try:
-                y_data, flows, *_ = self.model.eval(
-                    x_data,
-                    channels=channels,
-                    diameter=diam,
-                    net_avg=self.use_averaging.value,
-                    do_3D=x.volumetric,
-                    flow_threshold=self.flow_threshold.value,
-                    cellprob_threshold=self.dist_threshold.value
-                )
-            finally:
-                if self.use_gpu.value and self.model.torch:
-                    # Try to clear some GPU memory for other worker processes.
-                    try:
-                        from torch import cuda
-                        del self.model
-                        cuda.empty_cache()
-                        print("memory after clearing cache:")
-                        print(cuda.memory_reserved())
-                    except Exception as e:
-                        print(f"Unable to clear GPU memory. You may need to restart CellProfiler to change models. {e}")
 
-            y = Objects()
-            y.segmented = y_data
-            y.parent_image = x.parent_image
-            objects = workspace.object_set
-            objects.add_objects(y, y_name)
+        try:
+            y_data, flows, *_ = self.model.eval(
+                x_data,
+                channels=channels,
+                diameter=diam,
+                net_avg=self.use_averaging.value,
+                do_3D=x.volumetric,
+                flow_threshold=self.flow_threshold.value,
+                cellprob_threshold=self.dist_threshold.value
+            )
+        finally:
+            if self.use_gpu.value and self.model.torch:
+                # Try to clear some GPU memory for other worker processes.
+                try:
+                    from torch import cuda
+                    del self.model
+                    cuda.empty_cache()
+                    print("memory after clearing cache:")
+                    print(cuda.memory_reserved())
+                except Exception as e:
+                    print(f"Unable to clear GPU memory. You may need to restart CellProfiler to change models. {e}")
 
-            if self.save_probabilities.value:
-                # Flows come out sized relative to CellPose's inbuilt model size.
-                # We need to slightly resize to match the original image.
-                size_corrected = resize(flows[2], y_data.shape)
-                prob_image = Image(
-                    size_corrected,
-                    parent_image=x.parent_image,
-                    convert=False,
-                    dimensions=len(size_corrected.shape),
-                )
+        y = Objects()
+        y.segmented = y_data
+        y.parent_image = x.parent_image
+        objects = workspace.object_set
+        objects.add_objects(y, y_name)
 
-                workspace.image_set.add(self.probabilities_name.value, prob_image)
+        if self.save_probabilities.value:
+            # Flows come out sized relative to CellPose's inbuilt model size.
+            # We need to slightly resize to match the original image.
+            size_corrected = resize(flows[2], y_data.shape)
+            prob_image = Image(
+                size_corrected,
+                parent_image=x.parent_image,
+                convert=False,
+                dimensions=len(size_corrected.shape),
+            )
 
-                if self.show_window:
-                    workspace.display_data.probabilities = size_corrected
-
-            self.add_measurements(workspace)
+            workspace.image_set.add(self.probabilities_name.value, prob_image)
 
             if self.show_window:
-                if x.volumetric:
-                    # Can't show CellPose-accepted colour images in 3D
-                    workspace.display_data.x_data = x.pixel_data
-                else:
-                    workspace.display_data.x_data = x_data
-                workspace.display_data.y_data = y_data
-                workspace.display_data.dimensions = dimensions
+                workspace.display_data.probabilities = size_corrected
+
+        self.add_measurements(workspace)
+
+        if self.show_window:
+            if x.volumetric:
+                # Can't show CellPose-accepted colour images in 3D
+                workspace.display_data.x_data = x.pixel_data
+            else:
+                workspace.display_data.x_data = x_data
+            workspace.display_data.y_data = y_data
+            workspace.display_data.dimensions = dimensions
+            
+        lock.release()
 
     def display(self, workspace, figure):
         if self.save_probabilities.value:
