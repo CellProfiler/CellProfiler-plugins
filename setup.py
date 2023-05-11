f445da7f982d3183957f2e75c3b9ef069ba4f4dc
callum-jpg <callumtc.jpg@gmail.com>
2023-05-11 14:43:40 -0400

    initial testing workflow

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import setuptools
+
+install_deps = [
+    "cellprofiler~=4.2.5",
+    "cellprofiler-core~=4.2.5",
+            ]
+
+cellpose_deps = [
+    "cellpose~=2.2"
+]
+
+omnipose_deps = [
+    "omnipose"
+]
+
+stardist_deps = [
+    "stardist"
+]
+
+    
+setup(
+    name="cellprofiler_plugins",
+    packages=setuptools.find_packages(),
+    install_requires = install_deps,
+    extras_require = {
+      "cellpose": cellpose_deps,
+      "omnipose": omnipose_deps,
+      "stardist": stardist_deps,
+    }
+)
