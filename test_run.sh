d610390433e7b90bb88d7e45ea477507eb277479
callum-jpg <callumtc.jpg@gmail.com>
2023-05-30 14:21:34 -0400

    alternative pattern - check for pipeline stdout

--- test_run.sh
+++ test_run.sh
@@ -3,10 +3,10 @@
 # if grep -q "$1" "$2"
 if grep -qiE "$1" "$2"
 then
-   echo "Failed to load plugin"
-   `exit 1`
-else
     echo "Pipeline ran successfully"
     `exit 0`
+else
+   echo "Failed to run pipeline ($1 failed)"
+   `exit 1`
 fi 
 
