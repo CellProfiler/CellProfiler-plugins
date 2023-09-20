12caf50ee2abcf48693876aa7fb97ac4b3702143
bethac07 <bcimini@broadinstitute.org>
2023-09-20 11:41:46 -0400

    Allow pip installation, but also CellProfiler loading

--- setup.py
+++ setup.py
@@ -1,37 +1,41 @@
 from setuptools import setup
 import setuptools
 
-install_deps = [
-    "cellprofiler",
-    "cellprofiler-core",
-            ]
+if __name__!="__main__":
+    print("Please change your plugins folder to the 'active plugins' subfolder")
 
-cellpose_deps = [
-    "cellpose>=1.0.2"
-]
+else:
+    install_deps = [
+        "cellprofiler",
+        "cellprofiler-core",
+                ]
 
-omnipose_deps = [
-    "omnipose",
-    "ncolor"
-]
+    cellpose_deps = [
+        "cellpose>=1.0.2"
+    ]
 
-stardist_deps = [
-    "tensorflow",
-    "stardist"
-]
+    omnipose_deps = [
+        "omnipose",
+        "ncolor"
+    ]
 
-imagejscript_deps = [
-    "pyimagej"
-]
+    stardist_deps = [
+        "tensorflow",
+        "stardist"
+    ]
 
-setup(
-    name="cellprofiler_plugins",
-    packages=setuptools.find_packages(),
-    install_requires = install_deps,
-    extras_require = {
-      "cellpose": cellpose_deps,
-      "omnipose": omnipose_deps,
-      "stardist": stardist_deps,
-      "imagejscript": imagejscript_deps,
-    }
-)
+    imagejscript_deps = [
+        "pyimagej"
+    ]
+
+    setup(
+        name="cellprofiler_plugins",
+        packages=setuptools.find_packages(),
+        install_requires = install_deps,
+        extras_require = {
+          "cellpose": cellpose_deps,
+          "omnipose": omnipose_deps,
+          "stardist": stardist_deps,
+          "imagejscript": imagejscript_deps,
+        }
+    )
